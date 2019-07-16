7ce310940f704286f823c7b336aa2f2ea84182f6
Scott Stafford <scott.stafford@gmail.com>
2019-07-16 10:34:29 -0400

    Note dependency on 'future'.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.3",
+    version="1.0.4",
     description="A practical system for organizing validation rules",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -54,7 +54,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=['peppercorn'],
+    install_requires=['future'],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
