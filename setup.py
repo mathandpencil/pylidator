07f495fcff89b5f05331cdac2484c901f6634f81
andrew misiti <andrew.misiti@mathandpencil.com>
2026-06-17 11:52:14 -0400

    update setup.py and add titlecase to requirements

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["future"],
+    install_requires=open('requirements.txt').read(),
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
